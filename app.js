6fc811ba85c38c35da13c8b024c743f57765a7aa
Luica <138072443+lumartins311@users.noreply.github.com>
2023-08-04 12:29:59 +0000

    excusa

---
+++ app.js
@@ -0,0 +1,5 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+for (let i = 0; )
--- src/app.js
+++ src/app.js
@@ -7,5 +7,41 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let frase = excusa();
+  console.log(frase);
+  let html = document.getElementById("excuse");
+  html.innerHTML = frase;
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function excusa() {
+  let whoI = getRandomIntInclusive(0, who.length - 1);
+  let actionI = getRandomIntInclusive(0, action.length - 1);
+  let whatI = getRandomIntInclusive(0, what.length - 1);
+  let whenI = getRandomIntInclusive(0, when.length - 1);
+  let frase =
+    who[whoI] + " " + action[actionI] + " " + what[whatI] + " " + when[whenI];
+  console.log(whoI);
+  return frase;
+}
+
+// let whoI = who[Math.floor(Math.random() * who.lenght)];
+// let actionI = action[Math.floor(Math.random() * action.lenght)];
+// let whatI = what[Math.floor(Math.random() * what.lenght)];
+// let whenI = when[Math.floor(Math.random() * when.lenght)];
+// let frasefinal = whoI + " " + actionI + " " + whatI + " " + whenI;
